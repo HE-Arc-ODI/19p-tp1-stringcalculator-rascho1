620a2b4fc5dce0852152b1c346f0701d0cf911b0
rascho1 <40600851+rascho1@users.noreply.github.com>
2019-02-24 15:30:37 +0100

    corrrection methode, presque tout passe

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -12,33 +12,39 @@ import static java.lang.Integer.parseInt;
  * @author raphael.schoeni
  */
 public class StringCalculator {
-	private int sum;
+	private int sum = 0;
 	private int longu;
 	private String testeDelim;
 	private String endDelim;
 	private int pos;
 	private String delims;
+	private String reste;
 	
 	public StringCalculator(String number){
 	
 	
-	if (!"".equals(number)){
-		if (number.length()>1){
-			testeDelim = number.substring(2);
+	if (!" ".equals(number)){
+		if (number.length()>=2){
+			testeDelim = number.substring(0,2);
 			
 			
-				 if (!testeDelim.equals("//")){
-				 delims = "[,;\n]";
-					} else {
-			
-						pos = number.indexOf('\n');
-						/*if (pos > -1){*/
-						delims = number.substring(2,pos);}
-						String delimi = "[delim]";
-						String[] totDigits = number.split(delimi);
+				 if (testeDelim.equals("//")){
+						pos = number.indexOf(" ");
+						delims = number.substring(2,pos);	
+						reste = number.substring(pos+1);
+						
+						String delimi = "[delims]";
+						String[] totDigits = reste.split(delimi);
 						for (int i = 0; i < totDigits.length; i++) {
 							sum = sum+ parseInt(totDigits[i]);	
-							}}
+							}
+						
+					} else {
+						String delimi = "[,;\n]";
+						String[] totDigits = number.split(delimi);
+						for (int i = 0; i < totDigits.length; i++) {
+							sum = sum + parseInt(totDigits[i]);	
+							}}}
 		else if (number.length()==1){
 				sum = parseInt(number);
 			}
@@ -58,9 +64,18 @@ public class StringCalculator {
 	}
 		
 	
-	}
 	
+	}	
 public int getSum(){
 	return sum;
 }
+
+public static void main(String[] args) {
+		String teste2 = "Bonjour les";
+		int post = teste2.indexOf(" ");
+		String teste3 = teste2.substring(post+1);
+		int posisi = teste2.indexOf("B");
+		
+		System.out.println(teste3);
+	}
 }
