5300559dd2cb27c259aba55fadc67077e2a42a62
rascho1 <40600851+rascho1@users.noreply.github.com>
2019-02-24 13:16:15 +0100

    Ajout de la methode de teste ajout x delimiter et adapter methode
    Pour l'instant 4 testes ne passent plus

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -14,35 +14,53 @@ import static java.lang.Integer.parseInt;
 public class StringCalculator {
 	private int sum;
 	private int longu;
+	private String testeDelim;
+	private String endDelim;
+	private int pos;
+	private String delims;
 	
 	public StringCalculator(String number){
 	
 	
 	if (!"".equals(number)){
+		if (number.length()>1){
+			testeDelim = number.substring(2);
+			
+			
+				 if (!testeDelim.equals("//")){
+				 delims = "[,;\n]";
+					} else {
+			
+						pos = number.indexOf('\n');
+						/*if (pos > -1){*/
+						delims = number.substring(2,pos);}
+						String delimi = "[delim]";
+						String[] totDigits = number.split(delimi);
+						for (int i = 0; i < totDigits.length; i++) {
+							sum = sum+ parseInt(totDigits[i]);	
+							}}
+		else if (number.length()==1){
+				sum = parseInt(number);
+			}
 		
 		
-		String delims = "[,;\n]";
+	}
+	/*	String delims = "[,;\n]";
 		String[] totDigits = number.split(delims);
 		for (int i = 0; i < totDigits.length; i++) {
 			sum = sum+ parseInt(totDigits[i]);
 			
-		}
+		}*/
+	
 
-
-	}	else if ("".equals(number)){
+	else if ("".equals(number)){
 		sum =0;
 	}
 		
-			
-		}
 	
-
-	
-	
-	
+	}
 	
 public int getSum(){
 	return sum;
 }
-	
 }
